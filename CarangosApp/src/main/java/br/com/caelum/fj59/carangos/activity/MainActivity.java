08561c069d17c34420720b9b5a15961375e0831b
Felipe Torres <felipe.gtorres@gmail.com>
2014-07-28 17:34:11 -0300

    Exercício 2.2

--- CarangosApp/src/main/java/br/com/caelum/fj59/carangos/activity/MainActivity.java
+++ CarangosApp/src/main/java/br/com/caelum/fj59/carangos/activity/MainActivity.java
@@ -1,5 +1,6 @@
 package br.com.caelum.fj59.carangos.activity;
 
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.app.Activity;
 import android.widget.ListView;
@@ -11,34 +12,35 @@ import java.util.List;
 import br.com.caelum.fj59.carangos.R;
 import br.com.caelum.fj59.carangos.adapter.BlogPostAdapter;
 import br.com.caelum.fj59.carangos.app.CarangosApplication;
+import br.com.caelum.fj59.carangos.fragments.ListaDePostsFragment;
+import br.com.caelum.fj59.carangos.fragments.ProgressFragment;
 import br.com.caelum.fj59.carangos.modelo.BlogPost;
+import br.com.caelum.fj59.carangos.navegacao.EstadoMainActivity;
 import br.com.caelum.fj59.carangos.tasks.BuscaMaisPostsDelegate;
 import br.com.caelum.fj59.carangos.tasks.BuscaMaisPostsTask;
 
 public class MainActivity extends Activity implements BuscaMaisPostsDelegate{
-    private ListView postsList;
     private List<BlogPost> posts;
-    private BlogPostAdapter adapter;
+    private EstadoMainActivity estado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.posts_list);
+        setContentView(R.layout.main);
 
-        this.postsList = (ListView) findViewById(R.id.posts_list);
         this.posts = new ArrayList<BlogPost>();
-        this.adapter = new BlogPostAdapter(this, this.posts);
 
-        this.postsList.setAdapter(adapter);
-
-        new BuscaMaisPostsTask(this).execute();
+        this.estado = EstadoMainActivity.INICIO;
+        this.estado.executa(this);
     }
 
     @Override
     public void lidaComRetorno(List<BlogPost> posts) {
         this.posts.clear();
         this.posts.addAll(posts);
-        this.adapter.notifyDataSetChanged();
+
+        this.estado = EstadoMainActivity.PRIMEIROS_POSTS_RECEBIDOS;
+        this.estado.executa(this);
     }
 
     public List<BlogPost> getPosts() {
@@ -53,5 +55,14 @@ public class MainActivity extends Activity implements BuscaMaisPostsDelegate{
     @Override
     public CarangosApplication getCarangosApplication() {
         return (CarangosApplication) getApplication();
+    }
+
+    public void alteraEstadoEExecuta(EstadoMainActivity estado) {
+        this.estado = estado;
+        this.estado.executa(this);
+    }
+
+    public void buscaPrimeirosPosts() {
+        new BuscaMaisPostsTask(this).execute();
     }
 }
---
+++ CarangosApp/src/main/java/br/com/caelum/fj59/carangos/navegacao/EstadoMainActivity.java
@@ -0,0 +1,46 @@
+package br.com.caelum.fj59.carangos.navegacao;
+
+import android.app.Fragment;
+import android.app.FragmentTransaction;
+
+import br.com.caelum.fj59.carangos.R;
+import br.com.caelum.fj59.carangos.activity.MainActivity;
+import br.com.caelum.fj59.carangos.fragments.ListaDePostsFragment;
+import br.com.caelum.fj59.carangos.fragments.ProgressFragment;
+
+/**
+ * Created by felipe on 7/28/14.
+ */
+public enum EstadoMainActivity {
+    INICIO {
+        @Override
+        public void executa(MainActivity activity) {
+            activity.buscaPrimeirosPosts();
+            activity.alteraEstadoEExecuta(EstadoMainActivity.AGUARDANDO_POSTS);
+        }
+    },
+    AGUARDANDO_POSTS {
+        @Override
+        public void executa(MainActivity activity) {
+            ProgressFragment progress = ProgressFragment.comMensagem(R.string.hello_world);
+            this.colocaFragmentNaTela(activity, progress);
+        }
+    },
+    PRIMEIROS_POSTS_RECEBIDOS {
+        @Override
+        public void executa(MainActivity activity) {
+            this.colocaFragmentNaTela(activity, new ListaDePostsFragment());
+        }
+    };
+
+    void colocaFragmentNaTela(MainActivity activity, Fragment fragment) {
+
+        FragmentTransaction tx =
+                activity.getFragmentManager().beginTransaction();
+
+        tx.replace(R.id.fragment_principal, fragment);
+        tx.commit();
+    }
+
+    public abstract void executa(MainActivity activity);
+}

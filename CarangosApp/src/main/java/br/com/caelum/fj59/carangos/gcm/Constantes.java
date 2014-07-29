2240a5b1a943cc38a5c995b9d99ae922661639d6
Felipe Torres <felipe.gtorres@gmail.com>
2014-07-29 15:58:55 -0300

    Exercício 6.7

--- CarangosApp/src/main/java/br/com/caelum/fj59/carangos/gcm/Constantes.java
+++ CarangosApp/src/main/java/br/com/caelum/fj59/carangos/gcm/Constantes.java
@@ -5,4 +5,5 @@ package br.com.caelum.fj59.carangos.gcm;
  */
 public class Constantes {
     public static final String GCM_SERVER_ID = "290496985547";
+    public static final int ID_NOTIFICACAO = 123456;
 }
--- CarangosApp/src/main/java/br/com/caelum/fj59/carangos/gcm/GCMBroadcastReceiver.java
+++ CarangosApp/src/main/java/br/com/caelum/fj59/carangos/gcm/GCMBroadcastReceiver.java
@@ -1,9 +1,16 @@
 package br.com.caelum.fj59.carangos.gcm;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+
+import br.com.caelum.fj59.carangos.R;
+import br.com.caelum.fj59.carangos.activity.MainActivity;
 import br.com.caelum.fj59.carangos.infra.MyLog;
 
 /**
@@ -14,5 +21,28 @@ public class GCMBroadcastReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         MyLog.i("Chegou a mensagem do GCM!");
+
+        GoogleCloudMessaging gcm = GoogleCloudMessaging.getInstance(context);
+        String messageType = gcm.getMessageType(intent);
+
+        String mensagem = (String) intent.getExtras().getSerializable("message");
+
+        MyLog.i("Tipo da mensagem: " + messageType+ " Com conteúdo: " + mensagem);
+
+        Intent irParaLeilao = new Intent(context, MainActivity.class);
+        PendingIntent acaoPendente = PendingIntent.getActivity(context, 0, irParaLeilao, PendingIntent.FLAG_CANCEL_CURRENT);
+
+        irParaLeilao.putExtra("idDaNotificacao", Constantes.ID_NOTIFICACAO);
+
+        Notification notificacao = new Notification.Builder(context)
+                .setContentTitle("Um novo leilão comecou!")
+                .setContentText("Leilão: " + mensagem)
+                .setSmallIcon(R.drawable.ic_launcher)
+                .setContentIntent(acaoPendente)
+                .build();
+
+        NotificationManager manager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        manager.notify(Constantes.ID_NOTIFICACAO, notificacao);
     }
 }
